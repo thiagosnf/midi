06d8f1f9e7dba01f29089156f49e408836da9a01
Thiago Neves <thiago-snf@hotmail.com>
2022-06-03 05:44:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function tocaSom(seletorDeAudio) {
+  const elemento = document.querySelector(seletorDeAudio);
+
+  if (elemento && elemento.localName ==='audio') {
+      elemento.play();
+  }
+  else {
+    alert('Elemento não encontrado ou seletor inválido.');
+  }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function (evento) {
+    if(evento.code ==="Enter" || evento.code ==="Space") {
+      tecla.classList.add("ativa");
+    }
+    
+    tecla.onkeyup = function () {
+      tecla.classList.remove("ativa");
+    }
+    
+  }
+
+}
